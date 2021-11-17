d493ceec0491984974b8b30b976ae1167ad3b8d3
FlorianeFourner <floriane.fournier@ens-lyon.fr>
2021-11-17 21:19:10 +0000

    déplacer les scripts et push dans git

---
+++ Script/analyse_qualite.R
@@ -0,0 +1,8 @@
+#analyse qualité avec fastqc
+
+fastqc Data/* #analyse toutes les données 
+fastqc Data/2019_007_S7_R1.fastq #analyse juste pour une donnée afin de savoir comment ce sera nommée et aussi où il sera enregistré
+
+
+#pour executer un script bash chemin du script
+mv ./Data/*fastqc* Fastqc
