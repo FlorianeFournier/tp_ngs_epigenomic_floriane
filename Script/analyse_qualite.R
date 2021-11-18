fa2e69dfa3fc42ba29d3363e0b4d97e13eb37f3d
FlorianeFourner <floriane.fournier@ens-lyon.fr>
2021-11-18 12:52:40 +0000

    script analyse qualité

--- Script/analyse_qualite.R
+++ Script/analyse_qualite.R
@@ -1,8 +1,29 @@
 #analyse qualité avec fastqc
 
-fastqc Data/* #analyse toutes les données 
-fastqc Data/2019_007_S7_R1.fastq #analyse juste pour une donnée afin de savoir comment ce sera nommée et aussi où il sera enregistré
+#fastqc Data/* #analyse toutes les données 
+#fastqc Data/2019_007_S7_R1.fastq #analyse juste pour une donnée afin de savoir comment ce sera nommée et aussi où il sera enregistré
 
 
 #pour executer un script bash chemin du script
-mv ./Data/*fastqc* Fastqc
+#mv ./Data/*fastqc* Fastqc
+
+
+#multiqc .
+
+#mv *multiqc_report* Data/
+  
+  
+
+#contrôle qualité après trimming
+  
+fastqc Trim/*
+mv .Trim/*fastqc* Fastqc_trim
+
+multiqc .
+
+mv *multiqc_report* Data/
+
+
+
+  
+
