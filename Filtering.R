644c38c16262e1477e7186ce8f2378ca400fb22c
FlorianeFourner <floriane.fournier@ens-lyon.fr>
2021-12-14 08:33:45 +0000

    commentaire sur le script

--- Filtering.R
+++ Filtering.R
@@ -7,7 +7,7 @@
 for mapfile in Mapping/*bam
 
 do
-  java -jar $PICARD MarkDuplicates \
+  java -jar $PICARD MarkDuplicates \ #pour marquer les duplicats de PCR afin de pouvoir les enlever
     I=${mapfile} \
     O=${mapfile/".bam"/"marked_duplicated.bam"} \
     M=${mapfile/".bam"/"marked_dup_metics.txt"}
@@ -18,13 +18,13 @@ done
 #enlever les régions filtré
 
 grep -v -E "Mt|Pt" Genoms_informations/TAIR10_selectedRegions.bed > TAIR10_selectedRegions_Mt_Pt.bed #toutes les lignes sauf Pt et Mt donc sans ADN mito + chloro
-mv *TAIR10* Genoms_informations/
+mv *TAIR10* Genoms_informations/ #enlever les régions aux genome mitochondrial et chloroplastique : grep pour chercher les chaines de caractère dans un fichier
   
 
 
 for mapfile in Mapping/*duplicated.bam
 do
-  samtools view -b ${mapfile} -o ${mapfile/".bam"/"filtered.bam"} \
+  samtools view -b ${mapfile} -o ${mapfile/".bam"/"filtered.bam"} \ #utilise samtools view pour ouvrir les fichier bam et on utilise -L pour afficher les reads mapper que dans des régions que l'on a spécifié
   -L Genoms_informations/TAIR10_selectedRegions_Mt_Pt.bed \
-  -F 1024 -f 3 -q 30
+  -F 1024 -f 3 -q 30 #on filtre les flags dans les séquences avec F et f : F pour exclure 1024: duplicat de PCR q pour la qualité et f : inclure 3: reads perré 
 done
