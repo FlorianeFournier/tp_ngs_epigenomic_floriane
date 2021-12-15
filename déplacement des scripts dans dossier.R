23f9fb7c585cc4f966f991468d8a04da3a9f10f2
Your Name <you@example.com>
2021-12-15 14:51:21 +0000

    déplacement des scripts dans le dossier script

--- déplacement des scripts dans dossier.R
+++ déplacement des scripts dans dossier.R
@@ -3,3 +3,11 @@
 mv *téléchargement* Script/
 mv *qualite* Script/
 mv *déplacement* Script/
+mv *closest* Script/
+mv *comparaison_racine_cellule* Script/
+mv *Filtering* Script/
+mv *Mapping* Script/
+mv *peak_calling* Script/
+mv *qualite_post_filtering* Script/
+mv *Triming* Script/
+  
